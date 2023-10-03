9bee42e63df3b29cc11e9c4383b120cd2d07a4fc
PedroBozzeta <108239149+PedroBozzeta@users.noreply.github.com>
2023-10-02 21:35:45 -0500

    Adding LevelEducation Model, Repository and Controller

---
+++ JazaniT1.Api/Controllers/Admins/LevelEducationController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JazaniT1.Api.Controllers.Admins
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LevelEducationController : ControllerBase
+    {
+        // GET: api/<LevelEducationController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<LevelEducationController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<LevelEducationController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LevelEducationController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LevelEducationController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ JazaniT1.Domain/Admins/Repositories/ILevelEducationRepository.cs
@@ -0,0 +1,13 @@
+﻿using Jazani.Domain.Admins.Models;
+using System;
+
+namespace Jazani.Domain.Admins.Repositories
+{
+    //Definiendo el contrato de la Interfaz
+    public interface IOfficeRepository
+    {
+        Task<IReadOnlyList<LevelEducation>> FindAllAsync();
+        Task<LevelEducation?> FindByIdAsync(int id);
+        Task<LevelEducation?> SaveAsync(LevelEducation levelEducation);
+    }
+}
