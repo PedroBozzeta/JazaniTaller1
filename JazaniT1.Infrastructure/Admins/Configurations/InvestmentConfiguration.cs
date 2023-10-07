e4c5c2bb19125cc66c0d417768d89c384722556a
PedroBozzeta <108239149+PedroBozzeta@users.noreply.github.com>
2023-10-06 21:45:03 -0500

    Adding closing tags

--- JazaniT1.Infrastructure/Admins/Configurations/InvestmentConfiguration.cs
+++ JazaniT1.Infrastructure/Admins/Configurations/InvestmentConfiguration.cs
@@ -7,17 +7,6 @@ namespace JazaniT1.Infrastructure.Admins.Configurations
 {
     public class InvestmentConfiguration : IEntityTypeConfiguration<Investment>
     {
-        public int Id { get; set; }
-        public decimal AmountInvestd { get; set; } = 0;
-        public string? Description { get; set; }
-        public int? Year { get; set; }
-        public DateTime RegistrationDate { get; set; }
-        public bool State { get; set; }
-        public virtual Holder Holders { get; set; }
-        public virtual InvestmentConcept InvestmentConcepts { get; set; }
-        public virtual InvestmentType InvestmentTypes { get; set; }
-        public virtual MiningConcession MiningConcessions { get; set; }
-        public virtual PeriodType PeriodTypes { get; set; }
         public void Configure(EntityTypeBuilder<Investment> builder)
             {
                 builder.ToTable("investment", "mc");
@@ -41,10 +30,6 @@ namespace JazaniT1.Infrastructure.Admins.Configurations
                 .HasForeignKey(fk => fk.MiningConcessionId);
                 builder.HasOne(one => one.PeriodType).WithMany(many => many.Investments)
                 .HasForeignKey(fk => fk.PeriodTypeId);
-
         }
-        }
-    }
-}
     }
 }
